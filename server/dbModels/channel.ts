8afe93095d2389ec324fa2edc65e00119fcfe142
Saurav Raj <saurved785@gmail.com>
2022-09-23 23:23:29 +0530

    deleted files from main

--- server/dbModels/channel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ChannelsSchema = new mongoose.Schema(
-	{
-		name: { type: String, unique: true, required: true },
-	},
-	{ timestamps: true, strict: true, strictQuery: true }
-);
-export default mongoose.model("Channels", ChannelsSchema);
